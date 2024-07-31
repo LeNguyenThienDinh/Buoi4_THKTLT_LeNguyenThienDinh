bcbdf74e990d078e45e1efd2104ae792fcbc70df
ThienDinh <hellow12343@gmail.com>
2024-07-31 16:51:48 +0700

    hoan thanh bieu thuc 2 bai 1

--- DeQuy.cpp
+++ DeQuy.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <conio.h>
 #include <math.h>
 int Tongdequy(int n)
@@ -22,24 +22,52 @@ int khudequy(int n)
 	}
 	return tong;
 }
-
-int main()
+double Tongdequy_2(int n) 
 {
+	if (n == 1)
+		return 0.5;
+	return (double)n / (n + 1) + Tongdequy_2(n - 1);
+}
+
+double khudequy_2(int n) 
+{
+	double sum = 0;
+	for (int i = 1; i <= n; i++) 
+	{
+		sum += (double)i / (i + 1);
+	}
+	return sum;
+}
+
+int main() {
 	int n;
 	printf("Nhap so nguyen duong n: ");
+	
 	scanf("%d", &n);
 
-	if (n <= 0)
-	{
+	if (n <= 0) {
 		printf("Vui long nhap so nguyen duong lon hon 0.\n");
 		return 1;
 	}
 
-	int ketqua_dequy = Tongdequy(n);
-	int ketqua_khudequy = khudequy(n);
+	int chon;
+	printf("Chon bieu thuc de tinh (1-5): ");
+	scanf("%d", &chon);
 
-	printf("Tong S(n) tinh bang de quy: %d\n", ketqua_dequy);
-	printf("Tong S(n) tinh bang khong de quy: %d\n", ketqua_khudequy);
+	switch (chon) {
+	case 1:
+		printf("Tong S(n) tinh bang de quy: %d\n", Tongdequy(n));
+		printf("Tong S(n) tinh bang khong de quy: %d\n", khudequy(n));
+		break;
+	case 2:
+		printf("Tong S(n) tinh bang de quy: %.6f\n", Tongdequy_2(n));
+		printf("Tong S(n) tinh bang khong de quy: %.6f\n", khudequy_2(n));
+		break;
+	
+	default:
+		printf("Lua chon khong dung!\n");
+		break;
+	}
 
 	printf("Nhan enter de thoat chuong trinh");
 	getchar();
